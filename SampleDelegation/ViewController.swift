0304af7b010be7c5b6cb78f0983e61a30b035d23
Lavanya-Adishyam <lavanya.adishyam@gmail.com>
2019-12-05 16:39:56 +0530

    Initial Commit

---
+++ SampleDelegation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleDelegation
+//
+//  Created by Lavanya on 05/12/19.
+//  Copyright © 2019 Lavanya Akurathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
